ffa68eebb12dabbd9abd0d34b52c34976e209f43
Su5eD <su5ed@outlook.com>
2022-05-05 21:30:52 +0200

    Allow configuring specific source sets

--- src/main/kotlin/wtf/gofancy/koremods/gradle/KoremodsGradlePlugin.kt
+++ src/main/kotlin/wtf/gofancy/koremods/gradle/KoremodsGradlePlugin.kt
@@ -29,8 +29,11 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.tasks.SourceSet
 import org.gradle.language.jvm.tasks.ProcessResources
+import wtf.gofancy.koremods.RawScriptPack
 import wtf.gofancy.koremods.scanPath
+import java.nio.file.Path
 import java.util.*
 import kotlin.io.path.pathString
 import kotlin.io.path.relativeTo
@@ -54,6 +57,8 @@ class KoremodsGradlePlugin : Plugin<Project> {
     }
 
     override fun apply(project: Project) {
+        val koremodsExtension = project.extensions.create(KoremodsGradleExtension.EXTENSION_NAME, KoremodsGradleExtension::class.java)
+
         val pluginProperties = Properties().also {
             it.load(javaClass.getResourceAsStream("/koremods-gradle.properties"))
         }
@@ -70,25 +75,51 @@ class KoremodsGradlePlugin : Plugin<Project> {
             .configure { it.extendsFrom(koremodsImplementation) }
 
         project.createConfigurationWithDependencies(SCRIPTING_COMPILE_DEPS_CONFIGURATION_NAME, scriptingCompileDeps) {
-            exclude(mapOf(
-                "group" to "org.jetbrains.kotlin",
-                "module" to "kotlin-stdlib"
-            ))
-            exclude(mapOf(
-                "group" to "org.jetbrains.kotlin",
-                "module" to "kotlin-reflect"
-            ))
+            exclude(
+                mapOf(
+                    "group" to "org.jetbrains.kotlin",
+                    "module" to "kotlin-stdlib"
+                )
+            )
+            exclude(
+                mapOf(
+                    "group" to "org.jetbrains.kotlin",
+                    "module" to "kotlin-reflect"
+                )
+            )
         }
         project.createConfigurationWithDependencies(SCRIPTING_RUNTIME_DEPS_CONFI6GURATION_NAME, scriptingRuntimeDeps)
 
-        val javaExt = project.extensions.getByType(JavaPluginExtension::class.java)
-        javaExt.sourceSets.asSequence()
-            .mapNotNull { sourceSet ->
-                sourceSet.resources.srcDirs.firstNotNullOfOrNull locatePack@{ resourceRoot ->
-                    val scriptPack = scanPath(resourceRoot.toPath())
-                    return@locatePack scriptPack?.let { Pair(sourceSet, it) }
+        project.afterEvaluate { proj -> koremodsExtension.apply(proj) }
+    }
+
+    private fun Project.createConfigurationWithDependencies(name: String, dependencies: Iterable<String>, action: Configuration.() -> Unit = {}) {
+        project.configurations.create(name) { conf ->
+            conf.dependencies += dependencies
+                .map(project.dependencies::create)
+
+            action(conf)
+        }
+    }
+
+    private fun locateScriptPacks(sourceSets: Iterable<SourceSet>, strict: Boolean): List<Pair<SourceSet, RawScriptPack<Path>>> {
+        return sourceSets.mapNotNull { sourceSet ->
+            sourceSet.resources.srcDirs.firstNotNullOfOrNull locatePack@{ resourceRoot ->
+                val scriptPack = scanPath(resourceRoot.toPath())
+                if (scriptPack == null && strict) {
+                    throw RuntimeException("Expected source set '${sourceSet.name}' to contain a koremods script pack, but it could not be found")
                 }
+                return@locatePack scriptPack?.let { Pair(sourceSet, it) }
             }
+        }
+    }
+
+    private fun KoremodsGradleExtension.apply(project: Project) {
+        val sources = sourceSets.ifEmpty { 
+            project.extensions.getByType(JavaPluginExtension::class.java).sourceSets
+        }
+        
+        locateScriptPacks(sources, sourceSets.isNotEmpty())
             .forEach { (sourceSet, scriptPack) ->
                 val taskName = sourceSet.getTaskName("compile", "koremodsScripts")
                 val outputDir = project.buildDir.resolve(taskName)
@@ -116,14 +147,5 @@ class KoremodsGradlePlugin : Plugin<Project> {
                     }
                 }
             }
-    }
-
-    private fun Project.createConfigurationWithDependencies(name: String, dependencies: Iterable<String>, action: Configuration.() -> Unit = {}) {
-        project.configurations.create(name) { conf ->
-            conf.dependencies += dependencies
-                .map(project.dependencies::create)
-            
-            action(conf)
-        }
     }
 }
