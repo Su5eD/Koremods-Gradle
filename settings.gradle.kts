23707b42d1b7fbc2abcf742fd304e6fb87a5b46c
Su5eD <su5ed@outlook.com>
2023-07-03 12:39:14 +0200

    Add gradle toolchain resolver plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("de.fayard.refreshVersions") version "0.51.+"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.+"
 }
 
 rootProject.name = "koremods-gradle"
